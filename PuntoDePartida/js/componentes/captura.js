8c5eeb746f41813e11420ab97f5955541e9362fc
ChTole <ch.ariel.tole@gmail.com>
2024-09-27 10:59:26 -0300

    Gamma iframe - Estilos quiz

--- PuntoDePartida/js/componentes/captura.js
+++ PuntoDePartida/js/componentes/captura.js
@@ -1,19 +1,27 @@
 // Botón de captura y descarga
 let descargar = function (fragmento) {
-        html2canvas(document.querySelector(fragmento))
-            .then(function(canvas) {
+    document.querySelector('#descarga').classList.add('oculto');
+    document.querySelector('iframe').classList.add('oculto');
+    html2canvas(document.querySelector(fragmento))
+        .then(function(canvas) {
 
-            // Convert the canvas to a data URL
-            const dataURL = canvas.toDataURL('image/png');
+        // Convert the canvas to a data URL
+        const dataURL = canvas.toDataURL('image/png');
 
-            // Create an anchor element
-            const link = document.createElement('a');
-            link.href = dataURL;
-            link.download = 'descarga.png';
-            
-            // Trigger a click event on the anchor to start the download
-            link.click();
-            });
+        // Fecha en nombre de archivo
+        let hoy = new Date(Date.now());
+        let fecha = `${hoy.getDate()}-${hoy.getMonth()}-${hoy.getFullYear()}`
+
+        // Create an anchor element
+        const link = document.createElement('a');
+        link.href = dataURL;
+        link.download = `ptoPartidaEF${fecha}.png`;
+        
+        // Trigger a click event on the anchor to start the download
+        link.click();
+        });
+    document.querySelector('#descarga').classList.remove('oculto');
+    document.querySelector('iframe').classList.remove('oculto');
 };
 
 
--- PuntoDePartida/js/main.js
+++ PuntoDePartida/js/main.js
@@ -1,3 +1,4 @@
+import { cambiarEstiloSeleccion } from "./componentes/checkItems.js";
 import { descargar } from "./componentes/captura.js";
 import { cargarDatos } from "./componentes/carga.js";
 import { mostrarRtaInmediata } from "./componentes/rtaInmediata.js";
@@ -8,6 +9,7 @@ const rutaCuestionario = './data/CuestionarioPuntoDePartida-Emprendedorismo.json
 window.addEventListener("DOMContentLoaded", evento => {
     cargarDatos(rutaCuestionario);
     setTimeout(mostrarRtaInmediata, 500);
+    setTimeout(cambiarEstiloSeleccion, 300)
 });
 
 let capturar = document.getElementById('descarga');
