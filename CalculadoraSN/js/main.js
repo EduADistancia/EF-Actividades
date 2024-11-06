e3647bf84e1f6ac5d6d4e6fdbfce0cd43ed0ddde
ChTole <ch.ariel.tole@gmail.com>
2024-11-06 10:17:53 -0300

    Modificación: Sin descarga M2(1-3-5-9-CalcSN)

--- CalculadoraSN/js/main.js
+++ CalculadoraSN/js/main.js
@@ -1,10 +1,4 @@
 import { calcularSueldoNeto } from "./componentes/calculadora.js";
-import { descargar } from "./componentes/captura.js";
 
 let calcular = document.querySelector('#calcularSueldo');
 calcular.addEventListener('click', calcularSueldoNeto);
-
-let capturar = document.getElementById('descarga');
-capturar.addEventListener('click', ev => {
-    descargar('#captura');
-});
